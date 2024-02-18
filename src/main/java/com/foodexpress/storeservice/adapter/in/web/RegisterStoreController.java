30d4b655eea52e93abd1f8c47a86a5a00c9fe302
cyeji <choyeji1591@gmail.com>
2024-02-18 19:27:58 +0900

    refactor : api prefix 제거

--- src/main/java/com/foodexpress/storeservice/adapter/in/web/RegisterStoreController.java
+++ src/main/java/com/foodexpress/storeservice/adapter/in/web/RegisterStoreController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/store")
+@RequestMapping("/store")
 @RequiredArgsConstructor
 public class RegisterStoreController {
 
