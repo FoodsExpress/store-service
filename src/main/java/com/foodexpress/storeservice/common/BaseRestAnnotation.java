d9c2a54328728c66d4b741ba2cf7699c3e45c21c
이승구 <leesg107@naver.com>
2024-02-18 22:39:53 +0900

    feat: api result 래퍼 유틸 추가

---
+++ src/main/java/com/foodexpress/storeservice/common/BaseRestAnnotation.java
@@ -0,0 +1,19 @@
+package com.foodexpress.storeservice.common;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@RestController
+@RequestMapping("/store")
+@Component
+public @interface BaseRestAnnotation {
+
+}
