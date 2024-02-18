4442dea1c996a27aacdba90239c6c5526307f8aa
이승구 <leesg107@naver.com>
2024-02-18 22:39:53 +0900

    feat: api result 래퍼 유틸 추가

--- src/main/java/com/foodexpress/storeservice/common/ExceptionAdvice.java
+++ src/main/java/com/foodexpress/storeservice/common/ExceptionAdvice.java
@@ -1,17 +1,23 @@
 package com.foodexpress.storeservice.common;
 
 import com.foodexpress.storeservice.common.exceptions.NotValidBusinessNumberException;
+import com.foodexpress.storeservice.common.util.ApiUtil;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static com.foodexpress.storeservice.common.util.ApiUtil.fail;
+
 public class ExceptionAdvice {
+
+    public ApiUtil.ApiResult<?> sendError(Exception e, HttpStatus status) {
+        return fail(e, status);
+    }
 
     @ExceptionHandler(NotValidBusinessNumberException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<?> notValidBusinessNumber(NotValidBusinessNumberException notValidBusinessNumberException) {
-        return null;
+    public ApiUtil.ApiResult<?> notValidBusinessNumber(NotValidBusinessNumberException notValidBusinessNumberException) {
+        return sendError(notValidBusinessNumberException, HttpStatus.BAD_REQUEST);
     }
 
 }
