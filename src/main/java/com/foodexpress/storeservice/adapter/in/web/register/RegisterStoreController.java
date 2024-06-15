e897e9fffb08405bf8c1b785c11ca205f70889ba
이승구 <leesg107@naver.com>
2024-06-15 17:53:32 +0900

    docs: 주석 추가

--- src/main/java/com/foodexpress/storeservice/adapter/in/web/register/RegisterStoreController.java
+++ src/main/java/com/foodexpress/storeservice/adapter/in/web/register/RegisterStoreController.java
@@ -3,7 +3,7 @@ package com.foodexpress.storeservice.adapter.in.web.register;
 import com.foodexpress.storeservice.application.port.in.RegisterStoreTimeCommand;
 import com.foodexpress.storeservice.application.port.in.RegisterStoreTimeUseCase;
 import com.foodexpress.storeservice.application.port.in.RegisterStoreUseCase;
-import com.foodexpress.storeservice.common.util.ApiUtil.ApiResult;
+import com.foodexpress.storeservice.common.util.ApiUtil;
 import com.foodexpress.storeservice.domain.store.Store;
 import com.foodexpress.storeservice.domain.storetime.StoreTime;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +15,11 @@ import java.util.List;
 
 import static com.foodexpress.storeservice.common.util.ApiUtil.success;
 
+/**
+ * 상점 등록 컨트롤러
+ *
+ * @author seunggu.lee
+ */
 @RestController
 @RequiredArgsConstructor
 public class RegisterStoreController {
@@ -22,8 +27,15 @@ public class RegisterStoreController {
     private final RegisterStoreUseCase registerStoreUseCase;
     private final RegisterStoreTimeUseCase registerStoreTimeUseCase;
 
+    /**
+     * 상점을 등록한다.
+     * 상점을 먼저 등록한 이후 상점의 대한 식별자를 갖고 상점의 시간대를 따로 저장한다.
+     *
+     * @param registerStoreRequest 상점 등록 정보
+     * @return 등록된 상점 정보
+     */
     @PostMapping("/store")
-    public ResponseEntity<RegisterStoreResponse> registerStore(@RequestBody RegisterStoreRequest registerStoreRequest) {
+    public ApiUtil.ApiResult<RegisterStoreResponse> registerStore(@RequestBody RegisterStoreRequest registerStoreRequest) {
         Store store = registerStoreUseCase.registerStore(registerStoreRequest.convertCommand());
         List<RegisterStoreTimeCommand> timeCommandList = registerStoreRequest.getStoreTimeList()
             .stream()
